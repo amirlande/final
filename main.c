a8b2f035ac017891eecaa7b343c4d9f306abcab1
amirlande <amirlande@gmail.com>
2018-07-31 16:42:55 +0300

    Merge branch 'master' of https://github.com/eranchriqui/final into Amir
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+
+
+void print(int x){
+    printf("%d", x);
+}
+
 int main() {
-    printf("Hello, World!\n");
-    printf("love you too\n");
-    printf("Amir Lande Blau");
+
     printf("eran wrote this");
+
+    int x = 0;
+    print(x);
     return 0;
 }
